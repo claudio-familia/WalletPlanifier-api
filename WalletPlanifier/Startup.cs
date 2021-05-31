76044d101df1b94b4db5f713dcd18395a412846d
Claudio Familia Morel <claudio.familia.morel@gmail.com>
2021-05-31 13:05:02 -0400

    Set swagger to main page

--- WalletPlanifier/Startup.cs
+++ WalletPlanifier/Startup.cs
@@ -74,6 +74,7 @@ namespace WalletPlanifier
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = String.Empty;
             });
 
             app.UseRouting();
