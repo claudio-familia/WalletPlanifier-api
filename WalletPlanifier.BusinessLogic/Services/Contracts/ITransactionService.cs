c906b23189ff8563c35e9bac8387d4a739c0412a
Claudio Familia Morel <claudio.familia.morel@gmail.com>
2021-06-10 12:20:20 -0400

    Create Transaction service

---
+++ WalletPlanifier.BusinessLogic/Services/Contracts/ITransactionService.cs
@@ -0,0 +1,9 @@
+﻿using WalletPlanifier.BusinessLogic.Dto;
+using WalletPlanifier.Domain.Transactions;
+
+namespace WalletPlanifier.BusinessLogic.Services.Contracts
+{
+    public interface ITransactionService : IBaseService<Transaction, TransactionDto>
+    {
+    }
+}
---
+++ WalletPlanifier.BusinessLogic/Services/Transactions/TransactionService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WalletPlanifier.BusinessLogic.Dto;
+using WalletPlanifier.BusinessLogic.Services.Contracts;
+using WalletPlanifier.DataAccess.Repositories.Contracts;
+using WalletPlanifier.Domain.Transactions;
+
+namespace WalletPlanifier.BusinessLogic.Services.Transactions
+{
+    public class TransactionService : BaseService<Transaction, TransactionDto>, ITransactionService
+    {
+        public TransactionService(IDataRepository<Transaction> dataRepository,
+                                  IMapper mapper) : base(dataRepository, mapper)
+        {
+        }
+    }
+}
