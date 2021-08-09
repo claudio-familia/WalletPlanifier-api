f55ebd130eb7bea5d5dba3058b38f3fa6db49812
Claudio Familia Morel <claudio.familia.morel@gmail.com>
2021-08-09 06:51:54 -0400

    Validate error retrieving user data

--- WalletPlanifier.BusinessLogic/Services/Transactions/DebtService.cs
+++ WalletPlanifier.BusinessLogic/Services/Transactions/DebtService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using WalletPlanifier.BusinessLogic.Dto;
 using WalletPlanifier.BusinessLogic.Services.Contracts;
@@ -15,6 +16,7 @@ namespace WalletPlanifier.BusinessLogic.Services.Transactions
         private readonly IDataRepository<Debt> dataRepository;
         private readonly IDataRepository<User> userRepository;
         private readonly IDataRepository<Wallet> walletRepository;
+        private readonly ICurrentUserService currentUser;
         private readonly IDataRepository<Domain.Transactions.Transaction> transactionRepository;
         private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
@@ -25,11 +27,12 @@ namespace WalletPlanifier.BusinessLogic.Services.Transactions
                            ICurrentUserService currentUser,
                            IDataRepository<Domain.Transactions.Transaction> transactionRepository,
                            IUnitOfWork unitOfWork,
-                           IMapper mapper) : base(dataRepository, mapper, currentUser)
+                           IMapper mapper) : base(dataRepository, mapper)
         {
             this.dataRepository = dataRepository;
             this.userRepository = userRepository;
             this.walletRepository = walletRepository;
+            this.currentUser = currentUser;
             this.transactionRepository = transactionRepository;
             this.unitOfWork = unitOfWork;
             this.mapper = mapper;
@@ -68,6 +71,22 @@ namespace WalletPlanifier.BusinessLogic.Services.Transactions
                 throw new Exception(ex.Message);
             }
 
+        }
+
+        public override IEnumerable<DebtDto> GetAll()
+        {
+            var result = repository.GetAll().Where(x => x.CreatorUserId == currentUser.UserId);
+
+            return mapper.Map<IEnumerable<DebtDto>>(result);
+        }
+
+        public override DebtDto Get(int id)
+        {
+            var result = dataRepository.Get(id);
+
+            if (result.CreatorUserId != currentUser.UserId) throw new TypeAccessException("This resource does not belong to the requester");
+
+            return mapper.Map<DebtDto>(result);
         }
     }
 }
